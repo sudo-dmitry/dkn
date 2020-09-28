959e0c93a29ae5730b547b91b07339619e73e19f
sudo-dmitry <dmitry.v.pletnev@gmail.com>
2020-09-28 22:39:55 +0300

    ticket updated

--- js/script.js
+++ js/script.js
@@ -1,26 +1,6 @@
 
 // Load more speakers
 
-// $(function() {
-//   $(".speakers__items").slice(0, 3).show();
-//   $("#load-speakers").click(function(e) {
-//     e.preventDefault();
-//     $(".speakers__items:hidden").show();
-//   });
-// });
-
-// Load more schedule
-
-// $(function () {
-//   $(".schedule__items").slice(0, 2).show();
-//   $("#load-schedule").click(function(e) {
-//     e.preventDefault();
-//     $(".schedule__items:hidden").show();
-//   });
-// });
-
-// Load more speakers
-
 (function () {
 
   let speakers = document.querySelector('.speakers-new');
