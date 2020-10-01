6bb76eb9f22f544a2870e366133da3c559d0983d
sudo-dmitry <dmitry.v.pletnev@gmail.com>
2020-10-01 10:50:32 +0300

    privacy policy link added, modal deleted

--- js/script.js
+++ js/script.js
@@ -1,1 +1,101 @@
-!function(){let e=document.querySelector(".speakers"),t=(document.querySelector(".speakers .container"),document.querySelector(".speakers__collapsed")),n=0,c=document.getElementById("speakers-toggler"),d=!1;t.style.width=t.clientWidth+"px",t.classList.add("check"),n=t.clientHeight,t.classList.remove("check"),t.style.width="auto",c.addEventListener("click",(function(){d=e.classList.contains("expanded"),d?(e.classList.remove("expanded"),t.style.maxHeight=null):(e.classList.add("expanded"),t.style.maxHeight=n+"px")}))}(),function(){let e=document.querySelector(".schedule"),t=document.querySelector(".schedule__collapsed"),n=0,c=document.getElementById("schedule-toggler"),d=!1;t.style.width=t.clientWidth+"px",t.classList.add("check"),n=t.clientHeight,t.classList.remove("check"),t.style.width="auto",c.addEventListener("click",(function(){d=e.classList.contains("expanded"),d?(e.classList.remove("expanded"),t.style.maxHeight=null):(e.classList.add("expanded"),t.style.maxHeight=n+"px")}))}(),function(){let e=document.getElementById("fname"),t=document.getElementById("lname"),n=document.getElementById("tel"),c=document.getElementById("email");function d(e){let t=document.getElementById(e);document.getElementById(e+"-small").value=t.value}e.addEventListener("keyup",(function(){d("fname")})),t.addEventListener("keyup",(function(){d("lname")})),n.addEventListener("keyup",(function(){d("tel")})),c.addEventListener("keyup",(function(){d("email")}))}();const burger=document.getElementById("burger"),items=document.querySelectorAll(".js-item");for(const e of items)e.onclick=()=>{burger.checked=!1};window.onclick=e=>{const t=document.getElementById("menuToggle");e.target===t||t.contains(e.target)||(burger.checked=!1)};
+
+// Load more speakers
+
+(function () {
+
+  let speakers = document.querySelector('.speakers');
+  let speakersContainer = document.querySelector('.speakers .container');
+  let speakersCollapsed = document.querySelector('.speakers__collapsed');
+  let speakersCollapsedHeight = 0;
+  let toggler = document.getElementById('speakers-toggler');
+  let isExpanded = false;
+  
+  // Check collapsed part height
+  speakersCollapsed.style.width = speakersCollapsed.clientWidth + 'px';
+  speakersCollapsed.classList.add('check');  
+  speakersCollapsedHeight = speakersCollapsed.clientHeight;
+  speakersCollapsed.classList.remove('check');
+  speakersCollapsed.style.width = 'auto';
+
+  toggler.addEventListener('click', function () {
+    isExpanded = speakers.classList.contains('expanded');
+    if (isExpanded) {
+      speakers.classList.remove('expanded');
+      speakersCollapsed.style.maxHeight = null;
+    } else {
+      speakers.classList.add('expanded');
+      speakersCollapsed.style.maxHeight = speakersCollapsedHeight + 'px';
+    }
+  })
+
+})();
+
+// Load more schedule
+
+(function () {
+
+  let schedule = document.querySelector('.schedule');
+  let scheduleCollapsed = document.querySelector('.schedule__collapsed');
+  let scheduleCollapsedHeight = 0;
+  let toggler = document.getElementById('schedule-toggler');
+  let isExpanded = false;
+  
+  // Check collapsed part height
+  scheduleCollapsed.style.width = scheduleCollapsed.clientWidth + 'px';
+  scheduleCollapsed.classList.add('check');  
+  scheduleCollapsedHeight = scheduleCollapsed.clientHeight;
+  scheduleCollapsed.classList.remove('check');
+  scheduleCollapsed.style.width = 'auto';
+
+  toggler.addEventListener('click', function () {
+    isExpanded = schedule.classList.contains('expanded');
+    if (isExpanded) {
+      schedule.classList.remove('expanded');
+      scheduleCollapsed.style.maxHeight = null;
+    } else {
+      schedule.classList.add('expanded');
+      scheduleCollapsed.style.maxHeight = scheduleCollapsedHeight + 'px';
+    }
+  })
+
+})();
+
+
+// Sync inputs
+
+(function () {
+  
+  let fname = document.getElementById('fname');
+  let lname = document.getElementById('lname');
+  let tel = document.getElementById('tel');
+  let email = document.getElementById('email');
+  
+  fname.addEventListener('keyup', function () { syncFields('fname') });
+  lname.addEventListener('keyup', function () { syncFields('lname') });
+  tel.addEventListener('keyup', function () { syncFields('tel') });
+  email.addEventListener('keyup', function () { syncFields('email') });
+  
+  function syncFields(name) {
+    let field = document.getElementById(name);
+    let copy = document.getElementById(name + '-small');
+    copy.value = field.value;
+  }
+
+})();
+
+// Burger Menu
+
+const burger = document.getElementById('burger');
+const items = document.querySelectorAll('.js-item');
+for (const item of items) {
+  item.onclick = () => {
+    burger.checked = false;
+  }
+}
+
+window.onclick = (e) => {
+  const menu = document.getElementById('menuToggle');
+  if (e.target !== menu && !menu.contains(e.target)) {
+    burger.checked = false;
+  }
+}
